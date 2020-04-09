778450585dcee90108a2184e3889a2a5880d408e
Anton Fedchenko <kompotik@gmail.com>
2020-04-10 01:03:49 +0300

    fix2

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import { Provider } from "./components/ui/Provider";
 
-const Route1 = React.lazy(() => import("./routes/Route1"));
-const Route2 = React.lazy(() => import("./routes/Route2"));
-const Route3 = React.lazy(() => import("./routes/Route3"));
+import Route1 from "./routes/Route1";
+import Route2 from "./routes/Route2";
+import Route3 from "./routes/Route3";
 
 export const App = () => {
   return (
