bbf499d87f2c8683bffd10887d6c6c690c444a04
Anton Fedchenko <kompotik@gmail.com>
2020-04-10 01:00:02 +0300

    remove clsx as it's not a requirement for a build failure

--- src/components/module/Component1.jsx
+++ src/components/module/Component1.jsx
@@ -1,9 +1,8 @@
-import clsx from 'clsx';
 import React from 'react';
 
 import * as styles from './Component1.treat';
 import * as styles1 from './Component2.treat';
 
 export const Component1 = () => {
-  return <div className={clsx(styles.root, styles1.container)}>123</div>;
+  return <div className={styles.root}>123</div>;
 };
